a48231df10f4731b616722fb5d3cf50f67677991
Kevin Zeng <zengk@google.com>
2025-05-05 06:38:53 +0000

    build zephyr app with gn
    rm -rf out;gn gen out ; ninja -v -C out build_zephyr_app

--- configure_cmake.py
+++ configure_cmake.py
@@ -7,14 +7,11 @@ def main():
   dir_path = os.path.dirname(os.path.realpath(__file__))
   os.environ['Zephyr_DIR'] = dir_path + "/zephyr"
 
-  # Setup modules
-  modules = "-DZEPHYR_MODULES="+dir_path+"/modules/hal/atmel;"+dir_path+"/modules/hal/cmsis"
-
   #Application
   app = dir_path + "/app/"
 
   # Run CMake
-  subprocess.run(['cmake', '-Bbuild_cmake', '-GNinja', modules, '-DBOARD=robokit1', app])
+  subprocess.run(['cmake', '-Bbuild_cmake', '-GNinja', '-DBOARD=robokit1', app])
 
 if __name__ == '__main__':
   sys.exit(main());
